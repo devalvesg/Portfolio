b670a01af31c54e351472f1bffa30f5b6829da9b
devalvesg <gabrieldamasceno881@outlook.com>
2023-05-20 17:30:25 -0300

    Criando Navbar responsiva

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var section2 = document.getElementById('sobreMim');
 var section3 = document.getElementById('projetos');
 var message = document.querySelector('.message');
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop >= section2.offsetTop && scrollTop < section3.offsetTop) {
@@ -11,3 +11,31 @@ window.addEventListener('scroll', function() {
     message.classList.remove('show');
   }
 });
+
+// const botao = document.getElementsByClassName('btn-header');
+const menu = document.getElementById('menu');
+const close = document.getElementById('close');
+const icon = document.getElementById('icon')
+
+function navBar() {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'flex'
+    icon.style.display = 'none'
+    close.style.display = 'flex'
+    icon.add("rotacionando");
+  }
+  else {
+    showOff()
+  }
+
+  menu.addEventListener('click', () => {
+    showOff()
+  })
+
+};
+
+function showOff() {
+  menu.style.display = 'none'
+  icon.style.display = 'flex'
+  close.style.display = 'none'
+}
